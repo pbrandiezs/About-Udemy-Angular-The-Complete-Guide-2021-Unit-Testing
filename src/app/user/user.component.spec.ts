986b8872c57decebb102dce72ae620644d44f949
Perry Brandiezs <pbrandiezs@gmail.com>
2021-09-23 09:59:31 -0700

    feat: Lesson 425 Testing Dependencies: Components and Services

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
+import { UserService } from "./user.service";
 
 describe('UserComponent', () => {
   let component: UserComponent;
@@ -21,5 +22,24 @@ describe('UserComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should use the user name from the service', () => {
+    let userService = fixture.debugElement.injector.get(UserService);
+    fixture.detectChanges();
+    expect(userService.user.name).toEqual(component.user.name);
+  })
+
+  it('should display the user name if user is logged in', () => {
+    component.isLoggedIn = true;
+    fixture.detectChanges();
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).toContain(component.user.name);
+  });
+
+  it('should not display the user name if user is not logged in', () => {
+    fixture.detectChanges();
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).not.toContain(component.user.name);
   });
 });
